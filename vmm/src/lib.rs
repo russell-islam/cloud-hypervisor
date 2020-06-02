c599b8606fee38ea29f0a28f7cf80d705b5e8e34
Muminul Islam <muislam@microsoft.com>
2020-06-01 19:29:54 -0700

    vmm: Integrate hypervisor crate
    Modified vmm, vfio, pci, arch related modules to use hypervisor crate.
    Add support of hyperisor , Vm. Vcpu trait into the vmm modules.
    Replace all the kvm related struct references with use of <dyn trait
    object>. Added hypervisor trait object to the vmm structure and changed
    the main.rs file to default use of KVM. Later when we add support of
    Microsoft HyperV we will add the conditional compilations so that the
    build has only one supported hypervisor.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -5,6 +5,7 @@
 
 extern crate anyhow;
 extern crate arc_swap;
+extern crate hypervisor;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
@@ -184,6 +185,7 @@ pub fn start_vmm_thread(
     api_sender: Sender<ApiRequest>,
     api_receiver: Receiver<ApiRequest>,
     seccomp_level: &SeccompLevel,
+    hypervisor: Arc<dyn hypervisor::Hypervisor>,
 ) -> Result<thread::JoinHandle<Result<()>>> {
     let http_api_event = api_event.try_clone().map_err(Error::EventFdClone)?;
 
@@ -203,7 +205,7 @@ pub fn start_vmm_thread(
             // Apply seccomp filter for VMM thread.
             SeccompFilter::apply(vmm_seccomp_filter).map_err(Error::ApplySeccompFilter)?;
 
-            let mut vmm = Vmm::new(vmm_version.to_string(), api_event, vmm_path)?;
+            let mut vmm = Vmm::new(vmm_version.to_string(), api_event, vmm_path, hypervisor)?;
 
             vmm.control_loop(Arc::new(api_receiver))
         })
@@ -224,10 +226,16 @@ pub struct Vmm {
     vm: Option<Vm>,
     vm_config: Option<Arc<Mutex<VmConfig>>>,
     vmm_path: PathBuf,
+    hypervisor: Arc<dyn hypervisor::Hypervisor>,
 }
 
 impl Vmm {
-    fn new(vmm_version: String, api_evt: EventFd, vmm_path: PathBuf) -> Result<Self> {
+    fn new(
+        vmm_version: String,
+        api_evt: EventFd,
+        vmm_path: PathBuf,
+        hypervisor: Arc<dyn hypervisor::Hypervisor>,
+    ) -> Result<Self> {
         let mut epoll = EpollContext::new().map_err(Error::Epoll)?;
         let exit_evt = EventFd::new(EFD_NONBLOCK).map_err(Error::EventFdCreate)?;
         let reset_evt = EventFd::new(EFD_NONBLOCK).map_err(Error::EventFdCreate)?;
@@ -257,6 +265,7 @@ impl Vmm {
             vm: None,
             vm_config: None,
             vmm_path,
+            hypervisor,
         })
     }
 
@@ -272,6 +281,7 @@ impl Vmm {
                     exit_evt,
                     reset_evt,
                     self.vmm_path.clone(),
+                    self.hypervisor.clone(),
                 )?;
                 self.vm = Some(vm);
             }
@@ -341,6 +351,7 @@ impl Vmm {
             self.vmm_path.clone(),
             source_url,
             restore_cfg.prefault,
+            self.hypervisor.clone(),
         )?;
         self.vm = Some(vm);
 
@@ -383,7 +394,13 @@ impl Vmm {
             if self.reset_evt.read().is_ok() {
                 warn!("Spurious second reset event received. Ignoring.");
             }
-            self.vm = Some(Vm::new(config, exit_evt, reset_evt, self.vmm_path.clone())?);
+            self.vm = Some(Vm::new(
+                config,
+                exit_evt,
+                reset_evt,
+                self.vmm_path.clone(),
+                self.hypervisor.clone(),
+            )?);
         }
 
         // Then we start the new VM.
