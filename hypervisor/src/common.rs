3f20a031d86d38af13b590b74d6525c2275ea595
Muminul Islam <muislam@microsoft.com>
2020-06-03 12:58:00 -0700

    hypervisor: Add Vm trait to support multiple hypervisor.
    This VM trait should be implemented by each underlying hyperviosr.
    Previously created hypervisor object should create the VM based on
    already selected hypervisor. This is just the trait definition. For each
    of supported hypervisor we need to implement the trait. Later we will
    implement this trait for KVM and Microsoft HyperV.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- hypervisor/src/common.rs
+++ hypervisor/src/common.rs
@@ -18,4 +18,10 @@ pub use {
     kvm_bindings::kvm_sregs as SpecialRegisters,
     kvm_bindings::kvm_vcpu_events as VcpuEvents,
     kvm_ioctls::VcpuExit,
+    kvm_bindings::kvm_create_device as CreateDevice,
+    kvm_bindings::kvm_irq_routing as IrqRouting,
+    kvm_ioctls::DeviceFd,
+    kvm_ioctls::IoEventAddress,
+    kvm_bindings::kvm_userspace_memory_region as MemoryRegion,
+
 };
