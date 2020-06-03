5abc991d3dbe30e7bb4b9b2399cd1374485e296e
Muminul Islam <muislam@microsoft.com>
2020-06-03 12:23:56 -0700

    hypervisor: Add Vcpu trait to support multiple hypervisor.
            This Vcpu trait should be implemented by each underlying hyperviosr.
            Previously created hypervisor object should create the VM based on
            already selected hypervisor and Vm object should craete this vcpu
            object based on same hyperviosr. Each of this object should be
            reference by trait object i.e <dyn Vcpu> .
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

---
+++ hypervisor/src/x86_64/mod.rs
@@ -0,0 +1,15 @@
+// Copyright © 2019 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2020, Microsoft  Corporation
+//
+
+///
+/// Export generically-named wrappers of kvm-bindings for Unix-based platforms
+///
+pub use {
+    kvm_bindings::kvm_lapic_state as LapicState,
+    kvm_bindings::kvm_xcrs as ExtendedControlRegisters, kvm_bindings::kvm_xsave as Xsave,
+    kvm_bindings::CpuId, kvm_bindings::Msrs as MsrEntries, kvm_ioctls::Cap, kvm_ioctls::Kvm,
+};
