c2de674adf954c906d939e34750cb4a990518510
Muminul Islam <muislam@microsoft.com>
2020-06-03 12:23:56 -0700

    hypervisor: Add Vcpu trait to support multiple hypervisor.
    This Vcpu trait should be implemented by each underlying hyperviosr.
    Previously created hypervisor object should create the VM based on
    already selected hypervisor and Vm object should craete this vcpu
    object based on same hyperviosr. Each of this object should be
    reference by trait object i.e <dyn Vcpu>.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- hypervisor/src/lib.rs
+++ hypervisor/src/lib.rs
@@ -6,3 +6,22 @@
 //
 // Copyright 2018-2019 CrowdStrike, Inc.
 //
+//
+
+//! A generic abstraction around hypervisor functionality
+//!
+//! This crate offers a trait abstraction for underlying hypervisors
+//!
+//! # Platform support
+//!
+//! - x86_64
+//! - arm64
+//!
+#[cfg(target_arch = "x86_64")]
+/// X86_64 related module
+pub mod x86_64;
+
+/// Common for both x86 and aarch64
+pub mod common;
+/// CPU related module
+mod cpu;
