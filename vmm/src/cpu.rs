russell-islam/cloud-hypervisor

commit bd0985542444d449be9c523961f22a21ceb31417

cpu.rs: Use direct return
error: returning the result of a `let` binding from a block

Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

vmm/src/cpu.rs
@@ -1213,15 +1213,13 @@ impl CpuManager {
     ) -> Result<Vec<Arc<Mutex<Vcpu>>>> {
         trace_scoped!("create_boot_vcpus");
 
-        let ret = self.create_vcpus(self.boot_vcpus(), snapshot);
-
         #[cfg(feature = "snp")]
         if self.snp_enabled {
             self.vm
                 .snp_init()
                 .map_err(|e| Error::InitializeSnp(e.into()))?;
         }
-        ret
+        self.create_vcpus(self.boot_vcpus(), snapshot)
     }
 
     // Starts all the vCPUs that the VM is booting with. Blocks until all vCPUs are running.

commit 66dd40b840cfe3f7f73761d8971043f5214f947d

vmm: Create vcpus before transitioning to secure state
This is a requirement for SEV-SNP.

Fixes: bd098554 ("cpu.rs: Use direct return")
Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

vmm/src/cpu.rs
@@ -1213,11 +1213,17 @@ impl CpuManager {
     ) -> Result<Vec<Arc<Mutex<Vcpu>>>> {
         trace_scoped!("create_boot_vcpus");
 
-        #[cfg(feature = "snp")]
+        // Note: Please do not change the ordering of creation of vcpu.
+        // For SEV-SNP VM it is a requirement CPUs are created before
+        // transitioning the VM into a secure state.
+        let ret = self.create_vcpus(self.boot_vcpus(), snapshot);
+
         if self.snp_enabled {
+            #[cfg(feature = "snp")]
             self.vm.snp_init().map_err(Error::InitializeSnp)?;
         }
-        self.create_vcpus(self.boot_vcpus(), snapshot)
+
+        ret
     }
 
     // Starts all the vCPUs that the VM is booting with. Blocks until all vCPUs are running.