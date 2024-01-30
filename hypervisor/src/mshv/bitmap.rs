6a64d0871e7f936fc89b3bd47673cf919e271287
Muminul Islam <muislam@microsoft.com>
2024-01-30 12:54:42 -0800

    hypervisor: mshv: impl AtomicBitmapOps for AtomicU64
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- hypervisor/src/mshv/bitmap.rs
+++ hypervisor/src/mshv/bitmap.rs
@@ -2,7 +2,10 @@
 //
 // Copyright © 2024, Microsoft Corporation
 //
-use core::sync::atomic::Ordering;
+use core::{
+    mem,
+    sync::atomic::{AtomicU64, Ordering},
+};
 
 // Define a trait for atomic bitmap operation
 pub trait AtomicBitmapOps {
@@ -18,4 +21,37 @@ pub trait AtomicBitmapOps {
     fn load_bit(&self, index: usize, _order: Ordering) -> bool;
     // Capacity without self
     fn capacity() -> usize;
+}
+
+impl AtomicBitmapOps for AtomicU64 {
+    fn bit_size(&self) -> usize {
+        8 * mem::size_of::<AtomicU64>()
+    }
+
+    fn get_bit(&self, index: usize) -> bool {
+        self.load_bit(index, Ordering::Acquire)
+    }
+
+    fn set_bit(&self, index: usize) {
+        self.fetch_or(1 << index, Ordering::SeqCst);
+    }
+
+    fn reset_bit(&self, index: usize) {
+        self.fetch_and(!(1 << index), Ordering::SeqCst);
+    }
+
+    fn load_bit(&self, index: usize, order: Ordering) -> bool {
+        if index >= self.bit_size() {
+            panic!(
+                "Index: {:?} is greater than size: {:?}",
+                index,
+                self.bit_size()
+            );
+        }
+        1 == 1 & (self.load(order) >> index)
+    }
+
+    fn capacity() -> usize {
+        8 * mem::size_of::<AtomicU64>()
+    }
 }
