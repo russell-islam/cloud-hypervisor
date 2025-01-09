877dfd3691d71f606f2e5a9db8b7162eebc60e64
Wei Liu <liuwe@microsoft.com>
2025-01-09 01:49:16 +0000

    block: vhdx: use C ABI-qualification for packed structures
    This fixes the following issue from beta Clippy:
    
    error: item uses `packed` representation without ABI-qualification
    Error:    --> block/src/vhdx/vhdx_header.rs:333:1
        |
    331 |   #[repr(packed)]
        |          ------ `packed` representation set here
    332 |   #[derive(Clone, Copy, Debug)]
    333 | / pub struct RegionTableEntry {
    334 | |     pub guid: Uuid,
    335 | |     pub file_offset: u64,
    336 | |     pub length: u32,
    337 | |     pub required: u32,
    338 | | }
        | |_^
        |
        = warning: unqualified `#[repr(packed)]` defaults to `#[repr(Rust, packed)]`, which has no stable ABI
        = help: qualify the desired ABI explicity via `#[repr(C, packed)]` or `#[repr(Rust, packed)]`
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#repr_packed_without_abi
    
    Signed-off-by: Wei Liu <liuwe@microsoft.com>
    (cherry picked from commit 5b29eba636c3c61ff5722da9201880fcec0f18d1)
    (cherry picked from commit 8e452a04d625755069697cfca9ff443d225a1766)

--- block/src/vhdx/vhdx_metadata.rs
+++ block/src/vhdx/vhdx_metadata.rs
@@ -255,7 +255,7 @@ impl DiskSpec {
     }
 }
 
-#[repr(packed)]
+#[repr(C, packed)]
 #[derive(Default, Debug, Clone, Copy)]
 struct MetadataTableHeader {
     signature: u64,
@@ -286,7 +286,7 @@ impl MetadataTableHeader {
     }
 }
 
-#[repr(packed)]
+#[repr(C, packed)]
 #[derive(Default, Debug, Clone, Copy)]
 pub struct MetadataTableEntry {
     item_id: Uuid,
